1b3e71bc35603aa0a0de74d295ff677213e6d736
Rahul Patni <rpatni@purdue.edu>
2016-07-21 13:27:28 -0400

    Adding new student model

---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; } // primnary key column of the database table
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        /*
+         * Navifation property. They hold other entities that are related to this entitiy
+         * Enrollments property of a student entity with hold all of the Enrollment entities that are related to that Student entitiyt
+         * If a given student row in the database has two related enrollment rows, the student's entitiy's enrollments navigation property will contain those two enrollment entities
+         */
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
