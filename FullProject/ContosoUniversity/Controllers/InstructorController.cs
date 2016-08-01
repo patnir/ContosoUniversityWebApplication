35a57567d672c52ab9888edca00f077d5287e095
Rahul Patni <rpatni@purdue.edu>
2016-08-01 15:59:30 -0400

    Updating delete confirmed method

--- FullProject/ContosoUniversity/Controllers/InstructorController.cs
+++ FullProject/ContosoUniversity/Controllers/InstructorController.cs
@@ -107,6 +107,7 @@ namespace ContosoUniversity.Controllers
                 .Include(i => i.Courses)
                 .Where(i => i.ID == id)
                 .Single();
+            PopulateAssignedCourseData(instructor);
             if (instructor == null)
             {
                 return HttpNotFound();
@@ -207,9 +208,24 @@ namespace ContosoUniversity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Instructor instructor = db.Instructors.Find(id);
+            Instructor instructor = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Where(i => i.ID == id)
+                .Single();
+
             db.Instructors.Remove(instructor);
+
+            var department = db.Departments
+                .Where(d => d.InstructorID == id)
+                .SingleOrDefault();
+
+            if (department != null)
+            {
+                department.InstructorID = null;
+            }
+
             db.SaveChanges();
+
             return RedirectToAction("Index");
         }
 
