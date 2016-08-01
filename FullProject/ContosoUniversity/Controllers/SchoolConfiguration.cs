63f7e2305f6d2b1aef69771d5f7851060b0e13de
Rahul Patni <rpatni@purdue.edu>
2016-08-01 13:11:25 -0400

    Adding full project where future changes will be made

---
+++ FullProject/ContosoUniversity/Controllers/SchoolConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace ContosoUniversity.Controllers
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
