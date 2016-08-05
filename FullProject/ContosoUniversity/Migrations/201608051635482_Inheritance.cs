fb97547632bbc6d0b2ca6a62d86f72a4339cd6eb
Rahul Patni <rpatni@purdue.edu>
2016-08-05 12:36:32 -0400

    Adding inheritance migration

---
+++ FullProject/ContosoUniversity/Migrations/201608051635482_Inheritance.cs
@@ -0,0 +1,38 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Inheritance : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Instructor", newName: "Person");
+            AddColumn("dbo.Person", "EnrollmentDate", c => c.DateTime());
+            AddColumn("dbo.Person", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+            AlterColumn("dbo.Person", "LastName", c => c.String(nullable: false, maxLength: 20));
+            AlterColumn("dbo.Person", "HireDate", c => c.DateTime());
+            DropTable("dbo.Student");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Student",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        LastName = c.String(nullable: false, maxLength: 50),
+                        FirstName = c.String(nullable: false, maxLength: 50),
+                        EnrollmentDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            AlterColumn("dbo.Person", "HireDate", c => c.DateTime(nullable: false));
+            AlterColumn("dbo.Person", "LastName", c => c.String(nullable: false, maxLength: 50));
+            DropColumn("dbo.Person", "Discriminator");
+            DropColumn("dbo.Person", "EnrollmentDate");
+            RenameTable(name: "dbo.Person", newName: "Instructor");
+        }
+    }
+}
