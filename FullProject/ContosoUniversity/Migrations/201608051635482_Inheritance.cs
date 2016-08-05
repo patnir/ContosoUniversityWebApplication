a6837ec0e016da22cec428c8e594e9bbf8a562dc
Rahul Patni <rpatni@purdue.edu>
2016-08-05 12:42:00 -0400

    Migrations file update
    Removes foreign key constraints and indexes that point to the Student
    table.
    Renames the Instructor table as Person and makes changes needed for it
    to store Student data:
    Adds nullable EnrollmentDate for students.
    Adds Discriminator column to indicate whether a row is for a student or
    an instructor.
    Makes HireDate nullable since student rows won't have hire dates.
    Adds a temporary field that will be used to update foreign keys that
    point to students. When you copy students into the Person table they'll
    get new primary key values.
    Copies data from the Student table into the Person table. This causes
    students to get assigned new primary key values.
    Fixes foreign key values that point to students.
    Re-creates foreign key constraints and indexes, now pointing them to the
    Person table.

--- FullProject/ContosoUniversity/Migrations/201608051635482_Inheritance.cs
+++ FullProject/ContosoUniversity/Migrations/201608051635482_Inheritance.cs
@@ -7,12 +7,29 @@ namespace ContosoUniversity.Migrations
     {
         public override void Up()
         {
+            DropForeignKey("dbo.Enrollment", "StudentID", "dbo.Student");
+            DropIndex("dbo.Enrollment", new[] { "StudentID" });
+
             RenameTable(name: "dbo.Instructor", newName: "Person");
             AddColumn("dbo.Person", "EnrollmentDate", c => c.DateTime());
-            AddColumn("dbo.Person", "Discriminator", c => c.String(nullable: false, maxLength: 128));
-            AlterColumn("dbo.Person", "LastName", c => c.String(nullable: false, maxLength: 20));
+            AddColumn("dbo.Person", "Discriminator", c => c.String(nullable: false, maxLength: 128, defaultValue: "Instructor"));
             AlterColumn("dbo.Person", "HireDate", c => c.DateTime());
+            AddColumn("dbo.Person", "OldId", c => c.Int(nullable: true));
+
+            // Copy existing Student data into new Person table.
+            Sql("INSERT INTO dbo.Person (LastName, FirstName, HireDate, EnrollmentDate, Discriminator, OldId) SELECT LastName, FirstName, null AS HireDate, EnrollmentDate, 'Student' AS Discriminator, ID AS OldId FROM dbo.Student");
+
+            // Fix up existing relationships to match new PK's.
+            Sql("UPDATE dbo.Enrollment SET StudentId = (SELECT ID FROM dbo.Person WHERE OldId = Enrollment.StudentId AND Discriminator = 'Student')");
+
+            // Remove temporary key
+            DropColumn("dbo.Person", "OldId");
+
             DropTable("dbo.Student");
+
+            // Re-create foreign keys and indexes pointing to new table.
+            AddForeignKey("dbo.Enrollment", "StudentID", "dbo.Person", "ID", cascadeDelete: true);
+            CreateIndex("dbo.Enrollment", "StudentID");
         }
         
         public override void Down()
